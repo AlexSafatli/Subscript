8e5b70b05cf5f3a59d7c6a38c9b81cfe0b5bbb13
Alex Safatli <safatli@cs.dal.ca>
2015-04-15 15:13:21 -0300

    Fixed dependency issue in SymbolTable definition.

--- Lexer/SymbolTable.h
+++ Lexer/SymbolTable.h
@@ -5,11 +5,14 @@
 #ifndef SUBSCRIPT_SYMBOLTABLE_H
 #define SUBSCRIPT_SYMBOLTABLE_H
 
+#include <unordered_map>
 #include "AttributeValue.h"
+
+using namespace std;
 
 class SymbolTable {
 private:
-    map<int,AttributeValue&> table;
+    unordered_map<int,AttributeValue> table;
 public:
     AttributeValue &getAttributeValueByIndex(int index) const;
 };
